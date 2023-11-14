36af618d7d28e9677c80de327ff6b57bafa0b180
Power Up Studio <76599999+PowerUpStudios@users.noreply.github.com>
2023-11-14 22:05:13 +0100

    Create usb.cpp

---
+++ usb.cpp
@@ -0,0 +1,41 @@
+#include "pxt.h"
+
+namespace USBPort {
+
+// Wrapper class for USB communication
+class USBCom {
+public:
+    USBCom() {
+        // Add initialization code for USB communication
+        // This is a placeholder and needs to be adapted based on the target device
+    }
+
+    void sendString(const char *data) {
+        // Add code to send the string over USB
+        // This is a placeholder and needs to be adapted based on the target device
+        uBit.serial.send(data);
+    }
+
+    const char *receiveString() {
+        // Add code to receive the string over USB
+        // This is a placeholder and needs to be adapted based on the target device
+        return "Placeholder received string";
+    }
+};
+
+// Global instance of the USBCommunication class
+USBCom usb;
+
+// Wrapper function to send a string over USB
+// This function will be exposed to TypeScript
+void sendString(StringData *data) {
+    usb.sendString(PXT_STRING_DATA(data));
+}
+
+// Wrapper function to receive a string over USB
+// This function will be exposed to TypeScript
+StringData* receiveString() {
+    return PXT_STRING_RET(usb.receiveString());
+}
+
+} // namespace usb
